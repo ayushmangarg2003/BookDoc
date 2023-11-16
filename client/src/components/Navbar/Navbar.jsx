358a93fc1839eeb3ae1a76634dc8f6eb0ed6ef7f
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-11-17 00:25:57 +0530

    Navbar Made

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
-
+import { Link } from 'react-router-dom';
 const Navbar = () => {
+    const [state, setState] = useState(false);
+    const handleClick = () => {
+        setState(!state)
+    }
+
     return (
-        <>
+        <div className='navbar-parent'>
             <div className="nav-left">
                 {/* Logo */}
+                <div className="logo-img"><i className="fa-solid fa-user-doctor" /></div>
+                <div className="logo-name">DocCare</div>
             </div>
-
-            <div className="nav-center">
+            {/*  */}
+            <div className={state ? "nav-center active" :"nav-center" }>
                 {/* Navlinks */}
+                <div className="navlink" onClick={handleClick} ><i className="fa-solid fa-house-user" /><Link className='navlink-link' to={'/'}>Home</Link></div>
+                <div className="navlink" onClick={handleClick} ><i className="fa-solid fa-user-doctor" /><Link className='navlink-link' to={'/doctors'}>Doctors</Link></div>
+                <div className="navlink" onClick={handleClick} ><i className="fa-solid fa-briefcase" /><Link className='navlink-link' to={'/services'}>Services</Link></div>
+                <div className="navlink" onClick={handleClick} ><i className="fa-solid fa-address-book" /><Link className='navlink-link' to={'/contact'}>Contact</Link></div>
             </div>
 
             <div className="nav-right">
                 {/* Login Btn */}
+                <div className="login-btn"><Link className='navbar-login-link' to={'/login'}>Login</Link></div>
+                <div className="hamburger-icon" onClick={handleClick}><i className={state ? "fa-solid fa-xmark" : "fa-solid fa-bars" }></i></div>
             </div>
-        </>
+        </div>
     )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='app-parent'>
+      <App />
+    </div>
   </React.StrictMode>,
 )
 
