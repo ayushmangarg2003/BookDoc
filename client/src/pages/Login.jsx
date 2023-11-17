711f89458df046dc5b420a6dedf904f8597eb0a2
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-11-17 18:18:30 +0530

    Register Page Made

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,8 +6,8 @@ const Login = () => {
     <div className="login-parent">
       <div className="login-form">
         <h1>Hello! <p>Welcome</p> Back</h1>
-        <input type="text" id='email' placeholder='Enter Email' />
-        <input type="text" id="password" placeholder='Enter Password' />
+        <input type="text" placeholder='Enter Email' />
+        <input type="password" placeholder='Enter Password' />
         <div className='login-btn'>Login</div>
         <div className='login-newhere'>New Here ? <Link className='login-register-redirect' to={'/register'}> Register</Link></div>
       </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import RegisterIMG from "../assets/images/signup.gif"
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="register-parent">
+      <div className="register-left">
+        <img src={RegisterIMG} className='register-image' alt="Sign Up" />
+      </div>
+      <div className="register-right">
+        <h1>Create an <p>Account</p></h1>
+        <input type="text" placeholder='Enter Name' />
+        <input type="text" placeholder='Enter Email' />
+        <input type="password" placeholder='Enter Password' />
+
+        <div className='register-labels'>
+          <label className='role-label'>
+            <select name="role">
+              <option value="patient">Patient</option>
+              <option value="doctor">Doctor</option>
+            </select>
+          </label>
+
+          <label className='role-label'>
+            <select name="gender">
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          </label>
+        </div>
+
+        <input className='image-input' type="file" id='profile-pic' />
+        <label htmlFor="profile-pic" accept=".jpg .png">Choose Image</label>
+
+        <div className='register-btn'>Register</div>
+
+        <div className='register-olduser'>Have an Account ? <Link className='register-login-redirect' to={'/login'}> Login</Link></div>
+      </div>
+    </div>
   )
 }
 
