bafcc634ccaa5122edb20553527b48f96fbf787e
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-11-20 20:10:57 +0530

    Textual Edits

--- client-cra/src/assets/utils.js
+++ client-cra/src/assets/utils.js
@@ -1,17 +1,18 @@
-export const INSTAGRAM = "";
-export const LINKEDIN = "";
-export const TWITTER = "";
-export const GITHUB = "";
+export const INSTAGRAM = "https://www.instagram.com/storyofayushman/";
+export const LINKEDIN = "https://www.linkedin.com/in/ayushmangarg/";
+export const TWITTER = "https://twitter.com/AyushmanGarg4";
+export const GITHUB = "https://github.com/ayushmangarg2003";
 
-export const COMPANY_NAME_1="Vital";
-export const COMPANY_NAME_2="EASE";
+export const COMPANY_NAME_1="Doc";
+export const COMPANY_NAME_2="CARE";
 export const COPYRIGHT = "Copyright @ All Rights Reserved";
-export const HISTORY = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni quaerat nulla harum doloribus temporibus ipsum dolor sit amet consectetur, adipisicing elit. Magni quaerat nulla harum doloribus temporibus inventore eveniet animi nobis, velit nesciunt.";
-export const MISSION = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni quaerat nulla harum doloribus temporibus inventore.";
-export const VISION = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni quaerat nulla harum.";
-export const HOME_HEAD = "We Make your Life Longer";
-export const HOME_DESC = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni quaerat nulla harum doloribus temporibus inventore.";
+export const HISTORY = "Founded in 2020, our HealthTech platform has evolved from a vision of accessible healthcare. From humble beginnings, we've grown into a trusted platform, prioritizing innovation and user-centric healthcare solutions.";
 
-export const YEARS = "30+ ";
-export const DOCTORS = "69+ ";
+export const MISSION = "Our mission is to empower individuals with accessible and personalized healthcare solutions. Through innovation and compassion, we strive to enhance well-being and foster a healthier world for all.";
+export const VISION = "Transforming healthcare through cutting-edge technology, fostering a world where well-being is prioritized and accessible to all.";
+export const HOME_HEAD = "Transforming Healthcare Through Technology";
+export const HOME_DESC = "Through innovation and compassion, we strive to enhance well-being and foster a healthier world for all.";
+
+export const YEARS = "3+ ";
+export const DOCTORS = "700+ ";
 export const SATISFACTION = "100% ";
